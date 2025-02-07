191995e112a302bcb34a54898873f16eaf198a8b
kishorekankanala <130674398+kishorekankanala@users.noreply.github.com>
2025-02-07 10:48:09 -0600

    Update ConcateOfTwoNumbers.java
    removed kankanala string in y variable

--- src/main/java/day1/ConcateOfTwoNumbers.java
+++ src/main/java/day1/ConcateOfTwoNumbers.java
@@ -3,7 +3,7 @@ package day1;
 public class ConcateOfTwoNumbers {
     public static void main(String[] args) {
         String x = "sai ";
-        String y = "kishore kankanala";
+        String y = "kishore";
         System.out.println(x.concat(y));
         System.out.println(x.concat("!"));
     }
